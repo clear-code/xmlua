1286c3249e226b50ccc2aaa79047908c6330e485
HorimotoYasuhiro <horimoto@clear-code.com>
2017-11-24 15:53:39 +0900

    Add a missing assertion

--- test/test-libxml2.lua
+++ test/test-libxml2.lua
@@ -25,27 +25,27 @@ TestLibxml2XMLBuffer = {}
 -- TODO
 TestLibxml2XMLSave = {}
 
-TestLibxml2XPath = {}
-function TestLibxml2XPath:test_create_doc()
-  local xml = [[
- <!DOCTYPE doc [
- <!ELEMENT doc (src | dest)*>
- <!ELEMENT src EMPTY>
- <!ELEMENT dest EMPTY>
- <!ATTLIST src ref IDREF #IMPLIED>
- <!ATTLIST dest id ID #IMPLIED>
- ]>
- <doc>
-   <src ref="foo"/>
-   <dest id="foo"/>
-   <src ref="foo"/>
- </doc>
-]]
--- TODO
--- Add assertion
-  libxml2.xmlParseMemory(xml)
+local function to_xml(document)
+  local buffer = libxml2.xmlBufferCreate()
+  local context = libxml2.xmlSaveToBuffer(buffer,
+                                          "UTF-8",
+                                          bit.bor(ffi.C.XML_SAVE_FORMAT,
+                                                  ffi.C.XML_SAVE_NO_DECL,
+                                                  ffi.C.XML_SAVE_AS_XML))
+  libxml2.xmlSaveDoc(context, document)
+  libxml2.xmlSaveClose(context)
+  return libxml2.xmlBufferGetContent(buffer)
 end
 
+TestLibxml2XML = {}
+function TestLibxml2XML:test_parse_valid()
+  local xml = "<root/>"
+  local document = libxml2.xmlParseMemory(xml)
+  luaunit.assertEquals(to_xml(document),
+                       "<root/>\n")
+end
+
+TestLibxml2XPath = {}
 function TestLibxml2XPath:test_xpath_new_context()
   local xml = [[
  <!DOCTYPE doc [

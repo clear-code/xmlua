1c1e276a21a08d2627aa7d3f87f8de7b6f69d2d4
HorimotoYasuhiro <horimoto@clear-code.com>
2017-11-24 12:09:07 +0900

    Extract xmlBuffer related declarations

--- xmlua/libxml2.lua
+++ xmlua/libxml2.lua
@@ -19,28 +19,6 @@ int htmlCtxtUseOptions(htmlParserCtxt *context, int options);
 
 int htmlParseDocument(htmlParserCtxt *context);
 
-
-typedef enum {
-    XML_BUFFER_ALLOC_DOUBLEIT,	/* double each time one need to grow */
-    XML_BUFFER_ALLOC_EXACT,	/* grow only to the minimal size */
-    XML_BUFFER_ALLOC_IMMUTABLE, /* immutable buffer */
-    XML_BUFFER_ALLOC_IO,	/* special allocation scheme used for I/O */
-    XML_BUFFER_ALLOC_HYBRID,	/* exact up to a threshold, and doubleit thereafter */
-    XML_BUFFER_ALLOC_BOUNDED	/* limit the upper size of the buffer */
-} xmlBufferAllocationScheme;
-
-typedef struct _xmlBuffer xmlBuffer;
-struct _xmlBuffer {
-    xmlChar *content;		/* The buffer content UTF8 */
-    unsigned int use;		/* The buffer size used */
-    unsigned int size;		/* The buffer size */
-    xmlBufferAllocationScheme alloc; /* The realloc method */
-    xmlChar *contentIO;		/* in IO mode we may have a different base */
-};
-
-xmlBuffer *xmlBufferCreate(void);
-void xmlBufferFree(xmlBuffer *buf);
-
 typedef struct _xmlSaveCtxt xmlSaveCtxt;
 typedef xmlSaveCtxt *xmlSaveCtxtPtr;
 

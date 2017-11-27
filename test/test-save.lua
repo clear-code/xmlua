5c26b3ba8afb23f92b1bf36e6bf59f44f9195671
Kouhei Sutou <kou@clear-code.com>
2017-11-27 09:24:42 +0900

    Add to_xml()

--- test/test-save.lua
+++ test/test-save.lua
@@ -12,11 +12,29 @@ function TestSave.test_to_html()
 ]])
 end
 
-function TestSave.test_to_html_options()
+function TestSave.test_to_html_encoding()
   local html = xmlua.HTML.parse("<html><head></head></html>")
   luaunit.assertEquals(html:to_html({encoding = "EUC-JP"}),
                        [[
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
 <html><head><meta http-equiv="Content-Type" content="text/html; charset=EUC-JP"></head></html>
+]])
+end
+
+function TestSave.test_to_xml()
+  local xml = xmlua.XML.parse("<root/>")
+  luaunit.assertEquals(xml:to_xml(),
+                       [[
+<?xml version="1.0" encoding="UTF-8"?>
+<root/>
+]])
+end
+
+function TestSave.test_to_xml_encoding()
+  local xml = xmlua.XML.parse("<root/>")
+  luaunit.assertEquals(xml:to_xml({encoding = "EUC-JP"}),
+                       [[
+<?xml version="1.0" encoding="EUC-JP"?>
+<root/>
 ]])
 end
--- xmlua/savable.lua
+++ xmlua/savable.lua
@@ -3,29 +3,42 @@ local Savable = {}
 local libxml2 = require("xmlua.libxml2")
 local ffi = require("ffi")
 
-function Savable.to_html(self, options)
+local function save(target, flags, failure_message, options)
   local buffer = libxml2.xmlBufferCreate()
   local encoding = "UTF-8"
   if options and options.encoding then
     encoding = options.encoding
   end
-  local context = libxml2.xmlSaveToBuffer(buffer,
-                                          encoding,
-                                          bit.bor(ffi.C.XML_SAVE_FORMAT,
-                                                  ffi.C.XML_SAVE_NO_DECL,
-                                                  ffi.C.XML_SAVE_NO_EMPTY,
-                                                  ffi.C.XML_SAVE_AS_HTML))
+  local context = libxml2.xmlSaveToBuffer(buffer, encoding, flags)
   local success
-  if self.document then
-    success = libxml2.xmlSaveDoc(context, self.document)
+  if target.document then
+    success = libxml2.xmlSaveDoc(context, target.document)
   else
-    success = libxml2.xmlSaveTree(context, self.node)
+    success = libxml2.xmlSaveTree(context, target.node)
   end
   libxml2.xmlSaveClose(context)
   if not success then
-    error({message = "failed to generate HTML string"})
+    error({message = failure_message})
   end
   return libxml2.xmlBufferGetContent(buffer)
+end
+
+function Savable.to_html(self, options)
+  return save(self,
+              bit.bor(ffi.C.XML_SAVE_FORMAT,
+                      ffi.C.XML_SAVE_NO_DECL,
+                      ffi.C.XML_SAVE_NO_EMPTY,
+                      ffi.C.XML_SAVE_AS_HTML),
+              "failed to generate HTML string",
+              options)
+end
+
+function Savable.to_xml(self, options)
+  return save(self,
+              bit.bor(ffi.C.XML_SAVE_FORMAT,
+                      ffi.C.XML_SAVE_AS_XML),
+              "failed to generate XML string",
+              options)
 end
 
 return Savable

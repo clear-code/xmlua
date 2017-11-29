5e4bd0c0f2eb3d14edcf06c17172add92a9ec097
Kouhei Sutou <kou@clear-code.com>
2017-11-29 16:28:06 +0900

    Add sample to use with cqueues.thread

---
+++ sample/parse-cqueues-thread.lua
@@ -0,0 +1,43 @@
+#!/usr/bin/env luajit
+
+local thread = require("cqueues.thread")
+
+local n = 10
+
+local workers = {}
+local connections = {}
+
+for i = 1, n do
+  local worker, connection = thread.start(function(connection)
+      local xmlua = require("xmlua")
+      for path in connection:lines("*l") do
+        local file = io.open(path)
+        local html = file:read("*all")
+        file:close()
+
+        local success, document = pcall(xmlua.HTML.parse, html)
+        if success then
+          print(path .. ": " .. document:search("//title")[1]:to_html())
+        else
+          local err = document
+          print(path .. ": Failed to parse: " .. err.message)
+        end
+      end
+  end)
+  table.insert(workers, worker)
+  table.insert(connections, connection)
+end
+
+for i = 1, #arg do
+  for _, connection in ipairs(connections) do
+    connection:write(arg[i] .. "\n")
+  end
+end
+
+for _, connection in ipairs(connections) do
+  connection:close()
+end
+
+for _, worker in ipairs(workers) do
+  worker:join()
+end

75b06ae0831538ea6947af236dc63cf4ba462df1
Kouhei Sutou <kou@clear-code.com>
2017-11-30 22:50:35 +0900

    Accept no title case

--- sample/parse-html-cqueues-thread.lua
+++ sample/parse-html-cqueues-thread.lua
@@ -26,7 +26,12 @@ for i = 1, n do
 
         local success, document = pcall(xmlua.HTML.parse, html)
         if success then
-          print(path .. ": " .. document:search("//title")[1]:to_html())
+          local title = document:search("//title")[1]
+          if title then
+            print(path .. ": " .. title:to_html())
+          else
+            print(path .. ": no title")
+          end
         else
           local message = document
           print(path .. ": Failed to parse: " .. message)

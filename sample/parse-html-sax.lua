e49310000da5f92227c7a2b0beaacf19ab0755c9
Yasuhiro Horimoto <horimoto@clear-code.com>
2018-03-01 13:04:07 +0900

    sample: stop to use namespace
    Because HTML doesn't namespace specification.

--- sample/parse-html-sax.lua
+++ sample/parse-html-sax.lua
@@ -8,24 +8,8 @@ local file = assert(io.open(path))
 local parser = xmlua.HTMLSAXParser.new()
 
 parser.start_element = function(local_name,
-                                prefix,
-                                uri,
-                                namespaces,
                                 attributes)
   print("Start element: " .. local_name)
-  if prefix then
-    print("  prefix: " .. prefix)
-  end
-  if uri then
-    print("  URI: " .. uri)
-  end
-  for namespace_prefix, namespace_uri in pairs(namespaces) do
-    if namespace_prefix  == "" then
-      print("  Default namespace: " .. namespace_uri)
-    else
-      print("  Namespace: " .. namespace_prefix .. ": " .. namespace_uri)
-    end
-  end
   if #attributes > 0 then
     print("  Attributes:")
     for i, attribute in pairs(attributes) do

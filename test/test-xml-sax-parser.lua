eaa6e064bbe83f65630f21d846c04f09704b2cb3
Yasuhiro Horimoto <horimoto@clear-code.com>
2018-03-05 18:36:03 +0900

    XMLSAXParser: support "start element" with no namespace

--- test/test-xml-sax-parser.lua
+++ test/test-xml-sax-parser.lua
@@ -18,6 +18,65 @@ function TestXMLSAXParser.test_start_document()
   local succeeded = parser:parse(xml)
   luaunit.assertEquals({succeeded, called}, {true, true})
 end
+--[[
+local function collect_start_element(chunk)
+  local parser = xmlua.XMLSAXParser.new()
+  local elements = {}
+  praser.start_element = function(local_name,
+                                  prefix,
+                                  uri,
+                                  namespaces,
+                                  attributes)
+    local element = {
+      local_name = local_name,
+      prefix = prefix,
+      uri = uri,
+      namespaces = namespaces,
+      name = name,
+      attributes = attributes,
+    }
+    table.insert(elements, element)
+  end
+  luaunit.assertEquals(parser:parse(chunk), true)
+  return elements
+end
+--]]
+local function collect_start_elements(chunk)
+  local parser = xmlua.XMLSAXParser.new()
+  local elements = {}
+  parser.start_element = function(local_name,
+                                  prefix,
+                                  uri,
+                                  namespaces,
+                                  attributes)
+    local element = {
+      local_name = local_name,
+      prefix = prefix,
+      uri = uri,
+      namespaces = namespaces,
+      name = name,
+      attributes = attributes,
+    }
+   table.insert(elements, element)
+  end
+  luaunit.assertEquals(parser:parse(chunk), true)
+  return elements
+end
+
+function TestXMLSAXParser.test_start_element_no_namespace()
+  local xml = [[
+<?xml version="1.0" encoding="UTF-8" ?>
+<xml>
+]]
+  local expected = {
+    {
+      local_name = "xml",
+      namespaces = {},
+      attributes = {},
+    },
+  }
+  luaunit.assertEquals(collect_start_elements(xml), expected)
+end
 
 function TestXMLSAXParser.test_end_document()
   local xml = [[
--- xmlua/xml-sax-parser.lua
+++ xmlua/xml-sax-parser.lua
@@ -2,6 +2,8 @@ local XMLSAXParser = {}
 
 local libxml2 = require("xmlua.libxml2")
 local ffi = require("ffi")
+local converter = require("xmlua.converter")
+local to_string = converter.to_string
 
 local methods = {}
 
@@ -19,6 +21,56 @@ local function create_start_document_callback(user_callback)
   return c_callback
 end
 
+local function create_start_element_callback(user_callback)
+  local callback = function(user_data,
+                            raw_local_name,
+                            raw_prefix,
+                            raw_uri,
+                            n_namespaces,
+                            raw_namespaces,
+                            n_attributes,
+                            n_defaulted,
+                            raw_attributes)
+    local namespaces = {}
+    for i = 1, n_namespaces do
+      local base_index = (2 * (i - 1))
+      local prefix = to_string(raw_namespaces[base_index + 0])
+      local uri = to_string(raw_namespaces[base_index + 1])
+      if prefix then
+        namespaces[prefix] = uri
+      else
+        namespaces[""] = uri
+      end
+    end
+    local attributes = {}
+    for i = 1, n_attributes + n_defaulted do
+      local base_index = (5 * (i - 1))
+      local raw_attribute_local_name = raw_attributes[base_index + 0]
+      local raw_attribute_prefix = raw_attributes[base_index + 1]
+      local raw_attribute_uri = raw_attributes[base_index + 2]
+      local raw_attribute_value = raw_attributes[base_index + 3]
+      local raw_attribute_end = raw_attributes[base_index + 4]
+      local attribute = {
+        local_name = to_string(raw_attribute_local_name),
+        prefix = to_string(raw_attribute_prefix),
+        uri = to_string(raw_attribute_uri),
+        value = to_string(raw_attribute_value,
+                          raw_attribute_end - raw_attribute_value),
+        is_default = i > n_attributes,
+      }
+      table.insert(attributes, attribute)
+    end
+    user_callback(to_string(raw_local_name),
+                  to_string(raw_prefix),
+                  to_string(raw_uri),
+                  namespaces,
+                  attributes)
+  end
+  local c_callback = ffi.cast("startElementNsSAX2Func", callback)
+  ffi.gc(c_callback, function() c_callback:free() end)
+  return c_callback
+end
+
 local function create_end_document_callback(user_callback)
   local callback = function(user_data)
     user_callback()
@@ -32,6 +84,9 @@ function metatable.__newindex(parser, key, value)
   if key == "start_document" then
     value = create_start_document_callback(value)
     parser.context.sax.startDocument = value
+  elseif key == "start_element" then
+    value = create_start_element_callback(value)
+    parser.context.sax.startElementNs = value
   elseif key == "end_document" then
     value = create_end_document_callback(value)
     parser.context.sax.endDocument = value

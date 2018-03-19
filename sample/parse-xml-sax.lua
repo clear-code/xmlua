8d526e5b4f6e0bdd3d8d5b75a3818e88deb75b94
Yasuhiro Horimoto <spasgate870@movie.ocn.ne.jp>
2018-03-19 22:59:41 +0900

    sample: add a sample to use "start_element"

--- sample/parse-xml-sax.lua
+++ sample/parse-xml-sax.lua
@@ -49,6 +49,44 @@ parser.reference = function(entity_name)
   print("Reference entity name: " .. entity_name)
 end
 
+parser.start_element = function(local_name,
+                                prefix,
+                                uri,
+                                namespaces,
+                                attributes)
+  print("Start element: " .. local_name)
+  if prefix then
+    print("  prefix: " .. prefix)
+  end
+  if uri then
+    print("  URI: " .. uri)
+  end
+  for namespace_prefix, namespace_uri in pairs(namespaces) do
+    if namespace_prefix  == "" then
+      print("  Default namespace: " .. namespace_uri)
+    else
+      print("  Namespace: " .. namespace_prefix .. ": " .. namespace_uri)
+    end
+  end
+  if attributes then
+    if #attributes > 0 then
+      print("  Attributes:")
+      for i, attribute in pairs(attributes) do
+        local name
+        if attribute.prefix then
+          name = attribute.prefix .. ":" .. attribute.local_name
+        else
+          name = attribute.local_name
+        end
+        if attribute.uri then
+          name = name .. "{" .. attribute.uri .. "}"
+        end
+        print("    " .. name .. ": " .. attribute.value)
+      end
+    end
+  end
+end
+
 parser.end_document = function()
   print("End document")
 end

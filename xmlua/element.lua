127e448ad8ba81e3e615b8177672bb29cae4d38c
Kouhei Sutou <kou@clear-code.com>
2018-03-30 17:06:52 +0900

    Extract common code

--- xmlua/element.lua
+++ xmlua/element.lua
@@ -148,15 +148,13 @@ local function remove_namespace(node, prefix)
   end
 end
 
-function methods.append_element(self, name, attributes)
+local function create_sub_element(document, node, name, attributes)
   local raw_element = nil
   local colon_start = name:find(":")
   if colon_start then
     local namespace_prefix = name:sub(0, colon_start - 1)
     local local_name = name:sub(colon_start + 1)
-    local namespace = libxml2.xmlSearchNs(self.document,
-                                          self.node,
-                                          namespace_prefix)
+    local namespace = libxml2.xmlSearchNs(document, node, namespace_prefix)
     if namespace then
       raw_element = libxml2.xmlNewNode(namespace, local_name)
     else
@@ -179,57 +177,33 @@ function methods.append_element(self, name, attributes)
   else
     raw_element = libxml2.xmlNewNode(nil, name)
   end
-  local new_element = Element.new(self.document, raw_element)
+  local element = Element.new(document, raw_element)
   if attributes then
     for name, value in pairs(attributes) do
-      new_element:set_attribute(name, value)
+      element:set_attribute(name, value)
     end
   end
-  if libxml2.xmlAddChild(self.node, new_element.node) then
-    return new_element
+  return element
+end
+
+function methods.append_element(self, name, attributes)
+  local sub_element = create_sub_element(self.document,
+                                         self.node,
+                                         name,
+                                         attributes)
+  if libxml2.xmlAddChild(self.node, sub_element.node) then
+    return sub_element
   end
 end
 
 function methods.insert_element(self, position, name, attributes)
-  raw_element = nil
-  local colon_start = name:find(":")
-  if colon_start then
-    local namespace_prefix = name:sub(0, colon_start - 1)
-    local local_name = name:sub(colon_start + 1)
-    local namespace = libxml2.xmlSearchNs(self.document,
-                                          self.node,
-                                          namespace_prefix)
-    if namespace then
-      raw_element = libxml2.xmlNewNode(namespace, local_name)
-    else
-      raw_element = libxml2.xmlNewNode(nil, local_name)
-
-      local new_namespace = nil
-      for attribute_name, uri in pairs(attributes) do
-        local _, prefix_start = attribute_name:find("xmlns:")
-        if prefix_start then
-          local new_namespace_prefix = attribute_name:sub(prefix_start + 1)
-          new_namespace = libxml2.xmlNewNs(raw_element,
-                                           uri,
-                                           new_namespace_prefix)
-          libxml2.xmlSetNs(raw_element, new_namespace)
-          attributes[attribute_name] = nil
-          break
-        end
-      end
-    end
-  else
-    raw_element = libxml2.xmlNewNode(nil, name)
-  end
-  local new_element = Element.new(self.document, raw_element)
-  if attributes then
-    for name, value in pairs(attributes) do
-      new_element:set_attribute(name, value)
-    end
-  end
+  local sub_element = create_sub_element(self.document,
+                                         self.node,
+                                         name,
+                                         attributes)
   if libxml2.xmlAddPrevSibling(self:children()[position].node,
-                               new_element.node) then
-    return new_element
+                               sub_element.node) then
+    return sub_element
   end
 end
 

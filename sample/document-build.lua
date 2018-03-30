5c733d559556598155d0dbdf7932c5ee1049dc99
Kouhei Sutou <kou@clear-code.com>
2018-03-30 19:37:44 +0900

    Add a sample for XML.build

--- sample/document-build.lua
+++ sample/document-build.lua
@@ -2,7 +2,95 @@
 
 local xmlua = require("xmlua")
 
+local tree
+local document
+
 -- Empty tree
-local tree = {}
-local document = xmlua.XML.build(tree)
+tree = {}
+document = xmlua.XML.build(tree)
 print(document:to_xml())
+-- <?xml version="1.0" encoding="UTF-8"?>
+
+-- Root only document
+tree = {"root"}
+document = xmlua.XML.build(tree)
+print(document:to_xml())
+-- <?xml version="1.0" encoding="UTF-8"?>
+-- <root/>
+
+-- Root with attributes
+tree = {
+  "root",
+  {
+    attribute1 = "value1",
+    attribute2 = "value2",
+  },
+}
+document = xmlua.XML.build(tree)
+print(document:to_xml())
+-- <?xml version="1.0" encoding="UTF-8"?>
+-- <root attribute2="value2" attribute1="value1"/>
+
+-- Root with namespaces
+tree = {
+  "example:root",
+  {
+    ["xmlns:example"] = "http://example.com/",
+    ["example:attribute"] = "with-namespace",
+    ["attribute"] = "without-namespace",
+  },
+}
+document = xmlua.XML.build(tree)
+print(document:to_xml())
+-- (Indented manually for readability)
+--
+-- <?xml version="1.0" encoding="UTF-8"?>
+-- <example:root
+--   xmlns:example="http://example.com/"
+--   attribute="without-namespace"
+--   example:attribute="with-namespace"/>
+
+-- Child elements
+tree = {
+  "root",
+  {},
+  {
+    "child",
+    {
+      attribute = "child-value",
+    },
+  },
+}
+document = xmlua.XML.build(tree)
+print(document:to_xml())
+-- <?xml version="1.0" encoding="UTF-8"?>
+-- <root>
+--   <child attribute="child-value"/>
+-- </root>
+
+-- Child elements
+tree = {
+  "root",
+  {},
+  "root text1",
+  {
+    "child",
+    {
+      attribute = "child-value",
+    },
+    "child-text",
+  },
+  "root text2",
+}
+document = xmlua.XML.build(tree)
+print(document:to_xml())
+-- (Indented manually for readability)
+--
+-- <?xml version="1.0" encoding="UTF-8"?>
+-- <root>
+--   root text1
+--   <child attribute="child-value">
+--     child-text
+--   </child>
+--   root text2
+-- </root>

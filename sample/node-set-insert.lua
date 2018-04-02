b14327074bb9acb7485ebc0d4ea8b74d874fe643
Kouhei Sutou <kou@clear-code.com>
2018-04-02 10:21:45 +0900

    sample: use more meaning full variable names

--- sample/node-set-insert.lua
+++ sample/node-set-insert.lua
@@ -15,26 +15,31 @@ local document = xmlua.XML.parse([[
 </xml>
 ]])
 
---Insert node
-local inserted_node_set = document:search("//title")
--- <title>This is test</title>
-local insert_node = document:search("//xml/contents/sub1")[1]
--- <sub1>sub1</sub1>
-inserted_node_set:insert(insert_node)
+-- Insert a node
 
-print(inserted_node_set:to_xml())
+local node_set = document:search("//title")
+-- <title>This is test</title>
+
+local sub1 = document:search("//xml/contents/sub1")[1]
+-- <sub1>sub1</sub1>
+
+node_set:insert(sub1)
+
+print(node_set:to_xml())
 -- <title>This is test</title><sub1>sub1</sub1>
 
--- Insert node with position
-local inserted_node_set = document:search("//xml/contents/*")
+-- Insert a node with position
+
+local node_set = document:search("//xml/contents/*")
 -- <sub1>sub1</sub1>
 -- <sub2>sub2</sub2>
 -- <sub3>sub3</sub3>
-local insert_node = document:search("//title")[1]
--- <title>This is test</title>
-inserted_node_set:insert(1, insert_node)
 
-print(inserted_node_set:to_xml())
+local title = document:search("//title")[1]
+-- <title>This is test</title>
+node_set:insert(1, title)
+
+print(node_set:to_xml())
 -- <title>This is test</title>
 -- <sub1>sub1</sub1>
 -- <sub2>sub2</sub2>
--- sample/node-set-remove.lua
+++ sample/node-set-remove.lua
@@ -15,28 +15,36 @@ local document = xmlua.XML.parse([[
 </xml>
 ]])
 
---Remove node
-local node_set = document:search("//xml/contents/*")
+-- Remove a node
+
+local subs = document:search("//xml/contents/*")
 -- <sub1>sub1</sub1>
--- <sub2>sub2</sub2>
--- <sub3>sub3</sub3>
-local removed_node = node_set:remove(node_set[1])
--- "<sub1>sub1</sub1>" is removed
-print(removed_node:to_xml())
--- <sub1>sub1</sub1>
-print(node_set:to_xml())
 -- <sub2>sub2</sub2>
 -- <sub3>sub3</sub3>
 
--- Remove node with position
-local node_set = document:search("//xml/contents/*")
+local sub1 = subs:remove(subs[1])
+-- "<sub1>sub1</sub1>" is removed
+
+print(sub1:to_xml())
+-- <sub1>sub1</sub1>
+
+print(subs:to_xml())
+-- <sub2>sub2</sub2>
+-- <sub3>sub3</sub3>
+
+-- Remove a node with position
+
+local subs = document:search("//xml/contents/*")
 -- <sub1>sub1</sub1>
 -- <sub2>sub2</sub2>
 -- <sub3>sub3</sub3>
-local removed_node = node_set:remove(2)
+
+local sub2 = subs:remove(2)
 -- "<sub2>sub2</sub2>" is removed
-print(removed_node:to_xml())
+
+print(sub2:to_xml())
 -- <sub2>sub2</sub2>
-print(node_set:to_xml())
+
+print(subs:to_xml())
 -- <sub1>sub1</sub1>
 -- <sub3>sub3</sub3>

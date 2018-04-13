8c5ba479822e72d694d8db6ccd57c93a31499fd9
Yasuhiro Horimoto <horimoto@clear-code.com>
2018-04-13 11:30:16 +0900

    sample: modify to a more useful use cases

--- sample/document-add-entity.lua
+++ sample/document-add-entity.lua
@@ -11,25 +11,26 @@ local xml = [[
 ]]
 
 local parser = xmlua.XMLSAXParser.new()
+parser.start_element = function()
+  local document = parser.document
+  -- Setting information for add a entity
+  local entity = {
+    name = "Sample",
+  -- Entity type list
+  --   XML_INTERNAL_GENERAL_ENTITY = 1
+  --   XML_EXTERNAL_GENERAL_PARSED_ENTITY = 2
+  --   XML_EXTERNAL_GENERAL_UNPARSED_ENTITY = 3
+  --   XML_INTERNAL_PARAMETER_ENTITY = 4
+  --   XML_EXTERNAL_PARAMETER_ENTITY = 5
+  --   XML_INTERNAL_PREDEFINED_ENTITY = 6
+    entity_type = 1,
+    external_id = "-//W3C//DTD XHTML 1.0 Transitional//EN",
+    system_id = "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd",
+    content = "This is test."
+  }
+  document:add_entity(entity)
+end
 parser:parse(xml)
-local document = parser.document
-
--- Setting information for add a entity
-local entity = {
-  name = "Sample",
--- Entity type list
---   XML_INTERNAL_GENERAL_ENTITY = 1
---   XML_EXTERNAL_GENERAL_PARSED_ENTITY = 2
---   XML_EXTERNAL_GENERAL_UNPARSED_ENTITY = 3
---   XML_INTERNAL_PARAMETER_ENTITY = 4
---   XML_EXTERNAL_PARAMETER_ENTITY = 5
---   XML_INTERNAL_PREDEFINED_ENTITY = 6
-  entity_type = 1,
-  external_id = "-//W3C//DTD XHTML 1.0 Transitional//EN",
-  system_id = "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd",
-  content = "This is test."
-}
-document:add_entity(entity)
 
 
 -- Add a new entity to document DTD external subset
@@ -43,22 +44,24 @@ local xml = [[
 
 local options = {parser_dtd_load = true}
 local parser = xmlua.XMLSAXParser.new(options)
-parser:parse(xml)
-local document = parser.document
 
--- Setting information for add entity
-local entity = {
-  name = "Sample",
--- Entity type list
---   XML_INTERNAL_GENERAL_ENTITY = 1
---   XML_EXTERNAL_GENERAL_PARSED_ENTITY = 2
---   XML_EXTERNAL_GENERAL_UNPARSED_ENTITY = 3
---   XML_INTERNAL_PARAMETER_ENTITY = 4
---   XML_EXTERNAL_PARAMETER_ENTITY = 5
---   XML_INTERNAL_PREDEFINED_ENTITY = 6
-  entity_type = 1,
-  external_id = "-//W3C//DTD XHTML 1.0 Transitional//EN",
-  system_id = "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd",
-  content = "This is test."
-}
-document:add_dtd_entity(entity)
+parser.start_element = function()
+  local document = parser.document
+  -- Setting information for add entity
+  local entity = {
+    name = "Sample",
+  -- Entity type list
+  --   XML_INTERNAL_GENERAL_ENTITY = 1
+  --   XML_EXTERNAL_GENERAL_PARSED_ENTITY = 2
+  --   XML_EXTERNAL_GENERAL_UNPARSED_ENTITY = 3
+  --   XML_INTERNAL_PARAMETER_ENTITY = 4
+  --   XML_EXTERNAL_PARAMETER_ENTITY = 5
+  --   XML_INTERNAL_PREDEFINED_ENTITY = 6
+    entity_type = 1,
+    external_id = "-//W3C//DTD XHTML 1.0 Transitional//EN",
+    system_id = "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd",
+    content = "This is test."
+  }
+  document:add_dtd_entity(entity)
+end
+parser:parse(xml)

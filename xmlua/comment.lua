72e5dde44fedd0e5cd19002f90e0b26fb69bba2a
Yasuhiro Horimoto <horimoto@clear-code.com>
2018-05-29 19:41:10 +0900

    Add comment node object

---
+++ xmlua/comment.lua
@@ -0,0 +1,22 @@
+local Comment = {}
+
+local Node = require("xmlua.node")
+
+local methods = {}
+local metatable = {}
+
+function metatable.__index(element, key)
+  return methods[key] or
+    Node[key]
+end
+
+function Comment.new(document, node)
+  local comment = {
+    document = document,
+    node = node,
+  }
+  setmetatable(comment, metatable)
+  return comment
+end
+
+return Comment

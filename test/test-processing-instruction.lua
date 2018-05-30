98ea3c5c62f43f382625837da1962e449e29b4ed
Yasuhiro Horimoto <horimoto@clear-code.com>
2018-05-30 16:15:29 +0900

    test: add test case for ProcessingInstruction:content()

--- test/test-processing-instruction.lua
+++ test/test-processing-instruction.lua
@@ -13,3 +13,14 @@ function TestProcessingInstruction.test_path()
   luaunit.assertEquals(pi[1]:path(),
                        "/processing-instruction('xml-stylesheet')")
 end
+
+function TestProcessingInstruction.test_content()
+  local document = xmlua.XML.parse([[
+<?xml version="1.0" encoding="UTF-8" ?>
+<?xml-stylesheet href="www.test.com/test-style.xsl" type="text/xsl" ?>
+<root/>
+]])
+  local pi = document:search("/processing-instruction()")
+  luaunit.assertEquals(pi[1]:content(),
+                       "href=\"www.test.com/test-style.xsl\" type=\"text/xsl\" ")
+end

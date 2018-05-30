4fd326961d2f8dd638f6c80c024460e18f8681e8
Yasuhiro Horimoto <horimoto@clear-code.com>
2018-05-30 16:10:35 +0900

    test: simplify

--- test/test-processing-instruction.lua
+++ test/test-processing-instruction.lua
@@ -7,7 +7,7 @@ function TestProcessingInstruction.test_path()
   local document = xmlua.XML.parse([[
 <?xml version="1.0" encoding="UTF-8" ?>
 <?xml-stylesheet href="www.test.com/test-style.xsl" type="text/xsl" ?>
-<root></root>
+<root/>
 ]])
   local pi = document:search("/processing-instruction()")
   luaunit.assertEquals(pi[1]:path(),

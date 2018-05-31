a3e1f2cc0b1bbd3f81674951fb1a2e40084a61d6
Yasuhiro Horimoto <horimoto@clear-code.com>
2018-05-31 17:45:13 +0900

    test: add test case for CDATASection:content()

--- test/test-cdata-section.lua
+++ test/test-cdata-section.lua
@@ -5,12 +5,18 @@ TestCDATASection = {}
 
 function TestCDATASection.test_path()
   local document = xmlua.XML.parse([=[
-<root>
-  <![CDATA[This is <CDATA>]]>
-</root>
+<root><![CDATA[This is <CDATA>]]></root>
 ]=])
   local cdata_section = document:search("/root/text()")
   luaunit.assertEquals(cdata_section[1]:path(),
-                       "/root/text()[1]")
+                       "/root/text()")
 end
 
+function TestCDATASection.test_content()
+  local document = xmlua.XML.parse([=[
+<root><![CDATA[This is <CDATA>]]></root>
+]=])
+  local cdata_section = document:search("/root/text()")
+  luaunit.assertEquals(cdata_section[1]:content(),
+                       "This is <CDATA>")
+end

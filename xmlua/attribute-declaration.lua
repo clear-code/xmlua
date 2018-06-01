bd230b7b94aa5186f0ad02f0bae1142e432b6dcb
Yasuhiro Horimoto <horimoto@clear-code.com>
2018-06-01 16:18:47 +0900

    Add AttributeDeclaration node object

---
+++ xmlua/attribute-declaration.lua
@@ -0,0 +1,22 @@
+local AttributeDeclaration = {}
+
+local Node = require("xmlua.node")
+
+local methods = {}
+local metatable = {}
+
+function metatable.__index(element, key)
+  return methods[key] or
+    Node[key]
+end
+
+function AttributeDeclaration.new(document, node)
+  local attribute_declaration = {
+    document = document,
+    node = node,
+  }
+  setmetatable(attribute_declaration, metatable)
+  return attribute_declaration
+end
+
+return AttributeDeclaration

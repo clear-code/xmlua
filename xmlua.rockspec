20ae9b9d945ee7f7e90c086e60622c52fad99526
Yasuhiro Horimoto <horimoto@clear-code.com>
2018-06-01 15:54:48 +0900

    Add NameSpaceDeclaration node object

--- xmlua.rockspec
+++ xmlua.rockspec
@@ -62,6 +62,7 @@ build = {
     ["xmlua.libxml2.xmlsave"] = "xmlua/libxml2/xmlsave.lua",
     ["xmlua.libxml2.xmlstring"] = "xmlua/libxml2/xmlstring.lua",
     ["xmlua.libxml2.xpath"] = "xmlua/libxml2/xpath.lua",
+    ["xmlua.namespace-declaration"] = "xmlua/namespace-declaration.lua",
     ["xmlua.node"] = "xmlua/node.lua",
     ["xmlua.node-set"] = "xmlua/node-set.lua",
     ["xmlua.processing-instruction"] = "xmlua/processing-instruction.lua",
---
+++ xmlua/namespace-declaration.lua
@@ -0,0 +1,22 @@
+local NameSpaceDeclaration = {}
+
+local Node = require("xmlua.node")
+
+local methods = {}
+local metatable = {}
+
+function metatable.__index(element, key)
+  return methods[key] or
+    Node[key]
+end
+
+function NameSpaceDeclaration.new(document, node)
+  local namespace_declaration = {
+    document = document,
+    node = node,
+  }
+  setmetatable(namespace_declaration, metatable)
+  return namespace_declaration
+end
+
+return NameSpaceDeclaration

df3808c673655a36be7fc270455b6c2039776fa5
Yasuhiro Horimoto <horimoto@clear-code.com>
2018-06-01 15:24:34 +0900

    Add Entity node object

---
+++ xmlua/entity.lua
@@ -0,0 +1,22 @@
+local Entity = {}
+
+local Node = require("xmlua.node")
+
+local methods = {}
+local metatable = {}
+
+function metatable.__index(element, key)
+  return methods[key] or
+    Node[key]
+end
+
+function Entity.new(document, node)
+  local entity = {
+    document = document,
+    node = node,
+  }
+  setmetatable(entity, metatable)
+  return entity
+end
+
+return Entity

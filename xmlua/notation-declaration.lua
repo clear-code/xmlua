302b98e85fafeab677bfbd792d6ce6f8c3dfcd30
Yasuhiro Horimoto <horimoto@clear-code.com>
2018-06-01 16:23:51 +0900

    Add NotationDeclaration node object

---
+++ xmlua/notation-declaration.lua
@@ -0,0 +1,22 @@
+local NotationDeclaration = {}
+
+local Node = require("xmlua.node")
+
+local methods = {}
+local metatable = {}
+
+function metatable.__index(element, key)
+  return methods[key] or
+    Node[key]
+end
+
+function NotationDeclaration.new(document, node)
+  local notation_declaration = {
+    document = document,
+    node = node,
+  }
+  setmetatable(notation_declaration, metatable)
+  return notation_declaration
+end
+
+return NotationDeclaration

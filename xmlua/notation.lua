0162718e95e29f9cc3e0db77ba2ff4c4fceafc75
Yasuhiro Horimoto <horimoto@clear-code.com>
2018-06-06 06:50:46 +0900

    Add Notation node object

---
+++ xmlua/notation.lua
@@ -0,0 +1,35 @@
+local Notation = {}
+
+local Node = require("xmlua.node")
+local ffi = require("ffi")
+
+local methods = {}
+local metatable = {}
+
+function metatable.__index(element, key)
+  return methods[key] or
+    Node[key]
+end
+
+function methods:name()
+  return ffi.string(self.node.name)
+end
+
+function methods:public_id()
+  return ffi.string(self.node.PublicID)
+end
+
+function methods:system_id()
+  return ffi.string(self.node.SystemID)
+end
+
+function Notation.new(document, node)
+  local notation = {
+    document = document,
+    node = node,
+  }
+  setmetatable(notation, metatable)
+  return notation
+end
+
+return Notation

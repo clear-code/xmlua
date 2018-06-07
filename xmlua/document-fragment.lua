06690731e7f738d55c8b6e4dd99e2d53c766171f
Yasuhiro Horimoto <horimoto@clear-code.com>
2018-06-07 10:23:17 +0900

    DocumentFragment: fix a bug that can't tell the difference Element and DocumentFragment.

--- xmlua/document-fragment.lua
+++ xmlua/document-fragment.lua
@@ -4,13 +4,21 @@ local Element = require("xmlua.element")
 
 local methods = {}
 local metatable = {}
+local document_fragment
 
 function metatable.__index(element, key)
-  return methods[key]
+  return methods[key] or
+  document_fragment.parent[key]
 end
 
 function DocumentFragment.new(document, node)
-  return Element.new(document, node)
+  document_fragment = {
+    document = document,
+    node = node,
+    parent = Element.new(document, node)
+  }
+  setmetatable(document_fragment, metatable)
+  return document_fragment
 end
 
 return DocumentFragment

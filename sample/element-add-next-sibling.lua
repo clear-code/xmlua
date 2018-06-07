cc6e7ca1e6c551d2436144fa31d23579e6c6e8e2
Yasuhiro Horimoto <horimoto@clear-code.com>
2018-06-07 18:04:36 +0900

    sample: add sample for "Element:add_next_sibling()"

---
+++ sample/element-add-next-sibling.lua
@@ -0,0 +1,21 @@
+#!/usr/bin/env luajit
+
+local xmlua = require("xmlua")
+
+local document = xmlua.XML.parse([[
+<?xml version="1.0" encoding="UTF-8"?>
+<root>
+  <child1/>
+</root>
+]])
+local root = document:root()
+local comment_node =
+  document:create_comment("This is comment!")
+
+local child = root:children()[1]
+child:add_next_sibling(comment_node)
+print(document:to_xml())
+--<?xml version="1.0" encoding="UTF-8"?>
+--<root>
+--  <child1/><!--This is comment!-->
+--</root>

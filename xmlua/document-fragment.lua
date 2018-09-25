2d48c55ef40c3e3216a36972cb82ce4e2ff12bd0
Kouhei Sutou <kou@clear-code.com>
2018-09-25 17:28:29 +0900

    Remove needless module variable

--- xmlua/document-fragment.lua
+++ xmlua/document-fragment.lua
@@ -4,15 +4,14 @@ local Element = require("xmlua.element")
 
 local methods = {}
 local metatable = {}
-local document_fragment
 
-function metatable.__index(element, key)
+function metatable.__index(document_fragment, key)
   return methods[key] or
     document_fragment.parent[key]
 end
 
 function DocumentFragment.new(document, node)
-  document_fragment = {
+  local document_fragment = {
     document = document,
     node = node,
     parent = Element.new(document, node)

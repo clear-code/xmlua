0be12119bee221f338c3e66d108434698ee9a5f6
Horimoto Yasuhiro <horimoto@clear-code.com>
2020-04-14 16:39:10 +0900

    Bump version % rake version:update VERSION=1.1.7

--- xmlua.lua
+++ xmlua.lua
@@ -1,6 +1,6 @@
 local xmlua = {}
 
-xmlua.VERSION = "1.1.6"
+xmlua.VERSION = "1.1.7"
 
 xmlua.libxml2 = require("xmlua.libxml2")
 xmlua.XML = require("xmlua.xml")
--- xmlua.rockspec
+++ xmlua.rockspec
@@ -1,6 +1,6 @@
 -- -*- lua -*-
 
-local package_version = "1.1.6"
+local package_version = "1.1.7"
 
 package = "XMLua"
 version = package_version .. "-0"

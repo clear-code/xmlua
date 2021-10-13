c6c1d7c6e37a3419c7354130d1a980bb8450351b
Horimoto Yasuhiro <horimoto@clear-code.com>
2021-10-13 17:27:11 +0900

    tree: add support xmlGetNsList

--- xmlua/libxml2/tree.lua
+++ xmlua/libxml2/tree.lua
@@ -318,6 +318,8 @@ xmlNsPtr xmlSearchNs(xmlDocPtr doc, xmlNodePtr node, const xmlChar *nameSpace);
 xmlNsPtr xmlSearchNsByHref(xmlDocPtr doc, xmlNodePtr node, const xmlChar *href);
 xmlNsPtr *xmlGetNsList(const xmlDoc *doc, const xmlNode *node);
 
+xmlNsPtr *xmlGetNsList(const xmlDoc * doc, const xmlNode * node);
+
 char *xmlGetNoNsProp(const xmlNode *node, const xmlChar *name);
 xmlChar *xmlGetNsProp(const xmlNode *node,
                       const xmlChar *name,

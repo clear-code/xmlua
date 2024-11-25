890c12bbe91a591f18f764de836336ee00d49b4a
Thijs Schreijer <thijs@thijsschreijer.nl>
2024-11-25 13:43:44 +0100

    use c14n definitions as-is

--- xmlua/libxml2/c14n.lua
+++ xmlua/libxml2/c14n.lua
@@ -1,12 +1,6 @@
 local ffi = require("ffi")
 
 ffi.cdef[[
-  typedef enum {
-    XML_C14N_1_0 = 0, /* Original C14N 1.0 spec */
-    XML_C14N_EXCLUSIVE_1_0 = 1, /* Exclusive C14N 1.0 spec */
-    XML_C14N_1_1 = 2, /*  C14N 1.1 spec */
-  } xmlC14NMode;
-
   typedef int (*xmlOutputWriteCallback) (void * context, const char * buffer, int len);
   typedef int (*xmlOutputCloseCallback) (void * context);
 
@@ -30,10 +24,24 @@ ffi.cdef[[
   xmlOutputBufferPtr xmlOutputBufferCreateBuffer	(xmlBufferPtr buffer, xmlCharEncodingHandlerPtr encoder);
   int xmlOutputBufferClose	(xmlOutputBufferPtr out);
 
-  int xmlC14NDocSaveTo	(xmlDocPtr doc,
-          xmlNodeSetPtr nodes,
-          int mode, /* a xmlC14NMode */
-          xmlChar **inclusive_ns_prefixes,
-          int with_comments,
-          xmlOutputBufferPtr buf);
+
+/*
+ * xmlC14NMode:
+ *
+ * Predefined values for C14N modes
+ *
+ */
+ typedef enum {
+  XML_C14N_1_0            = 0,    /* Original C14N 1.0 spec */
+  XML_C14N_EXCLUSIVE_1_0  = 1,    /* Exclusive C14N 1.0 spec */
+  XML_C14N_1_1            = 2     /* C14N 1.1 spec */
+} xmlC14NMode;
+
+int
+  xmlC14NDocSaveTo	(xmlDocPtr doc,
+         xmlNodeSetPtr nodes,
+         int mode, /* a xmlC14NMode */
+         xmlChar **inclusive_ns_prefixes,
+         int with_comments,
+         xmlOutputBufferPtr buf);
 ]]

e57e61d493648df62dff272999bc45b7e78933ad
Thijs Schreijer <thijs@thijsschreijer.nl>
2024-11-25 12:31:32 +0100

    feat(c14n): add exec c14n method

--- xmlua/c14n.lua
+++ xmlua/c14n.lua
@@ -3,6 +3,40 @@ local C14n = {}
 local libxml2 = require("xmlua.libxml2")
 local ffi = require("ffi")
 
+local DEFAULT_MODE = "C14N_EXCLUSIVE_1_0"
+
+local Attribute
+local AttributeDeclaration
+local CDATASection
+local Comment
+local Document
+local DocumentFragment
+local DocumentType
+local Element
+local ElementDeclaration
+local EntityDeclaration
+local NamespaceDeclaration
+local Notation
+local ProcessingInstruction
+local Text
+
+function C14n.lazy_load()
+  Attribute = require("xmlua.attribute")
+  AttributeDeclaration = require("xmlua.attribute-declaration")
+  Text = require("xmlua.text")
+  CDATASection = require("xmlua.cdata-section")
+  Comment = require("xmlua.comment")
+  Document = require("xmlua.document")
+  DocumentFragment = require("xmlua.document-fragment")
+  DocumentType = require("xmlua.document-type")
+  Element = require("xmlua.element")
+  ElementDeclaration = require("xmlua.element-declaration")
+  EntityDeclaration = require("xmlua.entity-declaration")
+  NamespaceDeclaration = require("xmlua.namespace-declaration")
+  Notation = require("xmlua.notation")
+  ProcessingInstruction = require("xmlua.processing-instruction")
+end
+
 
 
 local C14N_MODES = {
@@ -81,8 +115,8 @@ end
 -- @param with_comments if truthy, comments will be included (default: false)
 -- @return string containing canonicalized xml
 function C14n:c14n_save(nodes, mode, inclusive_ns_prefixes, with_comments)
-  if mode == nil then -- default is exclusive 1.0
-    mode = "C14N_EXCLUSIVE_1_0"
+  if mode == nil then
+    mode = DEFAULT_MODE
   end
   with_comments = with_comments and 1 or 0 -- default = not including comments
 
@@ -101,6 +135,123 @@ function C14n:c14n_save(nodes, mode, inclusive_ns_prefixes, with_comments)
   return libxml2.xmlBufferGetContent(buffer)
 end
 
+
+
+local wrap_raw_node do
+  -- order is according to the constant value of xmlElementType enum in libxml2
+  local type_generators = setmetatable({
+    [ffi.C.XML_ELEMENT_NODE] = function(document, raw_node)
+      return Element.new(document, raw_node)
+    end,
+    [ffi.C.XML_ATTRIBUTE_NODE] = function(document, raw_node)
+      return Attribute.new(document, raw_node)
+    end,
+    [ffi.C.XML_TEXT_NODE] = function(document, raw_node)
+      return Text.new(document, raw_node)
+    end,
+    [ffi.C.XML_CDATA_SECTION_NODE] = function(document, raw_node)
+      return CDATASection.new(document, raw_node)
+    end,
+    [ffi.C.XML_ENTITY_REF_NODE] = function(document, raw_node)
+      error("XML_ENTITY_REF_NODE not implemented")        -- TODO: implement
+    end,
+    [ffi.C.XML_ENTITY_NODE] = function(document, raw_node)
+      error("XML_ENTITY_NODE not implemented")            -- TODO: implement
+    end,
+    [ffi.C.XML_PI_NODE] = function(document, raw_node)
+      return ProcessingInstruction.new(document, raw_node)
+    end,
+    [ffi.C.XML_COMMENT_NODE] = function(document, raw_node)
+      return Comment.new(document, raw_node)
+    end,
+    [ffi.C.XML_DOCUMENT_NODE] = function(document, raw_node)
+      return Document.new(raw_node)
+    end,
+    [ffi.C.XML_DOCUMENT_TYPE_NODE] = function(document, raw_node)
+      return DocumentType.new(document, raw_node)
+    end,
+    [ffi.C.XML_DOCUMENT_FRAG_NODE] = function(document, raw_node)
+      return DocumentFragment.new(document, raw_node)
+    end,
+    [ffi.C.XML_NOTATION_NODE] = function(document, raw_node)
+      return Notation.new(document, raw_node)
+    end,
+    [ffi.C.XML_HTML_DOCUMENT_NODE] = function(document, raw_node)
+      error("XML_HTML_DOCUMENT_NODE not implemented")     -- TODO: implement
+    end,
+    [ffi.C.XML_DTD_NODE] = function(document, raw_node)
+      error("XML_DTD_NODE not implemented")               -- TODO: implement
+    end,
+    [ffi.C.XML_ELEMENT_DECL] = function(document, raw_node)
+      return ElementDeclaration.new(document, raw_node)
+    end,
+    [ffi.C.XML_ATTRIBUTE_DECL] = function(document, raw_node)
+      return AttributeDeclaration.new(document, raw_node)
+    end,
+    [ffi.C.XML_ENTITY_DECL] = function(document, raw_node)
+      return EntityDeclaration.new(document, raw_node)
+    end,
+    [ffi.C.XML_NAMESPACE_DECL] = function(document, raw_node)
+      return NamespaceDeclaration.new(document, raw_node)
+    end,
+    [ffi.C.XML_XINCLUDE_START] = function(document, raw_node)
+      error("XML_XINCLUDE_START not implemented")         -- TODO: implement
+    end,
+    [ffi.C.XML_XINCLUDE_END] = function(document, raw_node)
+      error("XML_XINCLUDE_END not implemented")           -- TODO: implement
+    end,
+    [ffi.C.XML_DOCB_DOCUMENT_NODE] = function(document, raw_node)
+      error("XML_DOCB_DOCUMENT_NODE not implemented")     -- TODO: implement
+    end,
+  }, {
+    __index = function(self, key)
+      error("Unknown node type: " .. tostring(key))
+    end
+  })
+
+  function wrap_raw_node(document, raw_node)
+    if raw_node == ffi.NULL then
+      return nil
+    end
+    return type_generators[tonumber(raw_node.type)](document, raw_node)
+  end
+end
+
+
+
+--- Canonicalise an xmlDocument or set of elements.
+-- @param self xmlDoc from which to canonicalize elements
+-- @param callback function to determine if a node should be included in the canonicalized output.
+--        Signature: `boolean = function(node, parent)`
+-- @param mode any of C14N_1_0, C14N_EXCLUSIVE_1_0 (default), C14N_1_1
+-- @param inclusive_ns_prefixes array, or space-separated string, of namespace prefixes to include
+-- @param with_comments if truthy, comments will be included (default: false)
+-- @return string containing canonicalized xml
+function C14n:c14n_execute(callback, mode, inclusive_ns_prefixes, with_comments)
+  if mode == nil then
+    mode = DEFAULT_MODE
+  end
+  with_comments = with_comments and 1 or 0 -- default = not including comments
+  assert(type(callback) == "function", "callback must be a function")
+
+  -- wrap the callback to pass wrapped objects, and return 1 or 0
+  local cbwrapper = function(_, nodePtr, parentPtr)
+    return callback(wrap_raw_node(self, nodePtr), wrap_raw_node(self, parentPtr)) and 1 or 0
+  end
+
+  mode = assert(C14N_MODES_LOOKUP[mode], "mode must be a valid C14N mode constant")
+  local prefixes = getNamespacePrefixArray(inclusive_ns_prefixes)
+  local buffer = libxml2.xmlBufferCreate()
+  local output_buffer = libxml2.xmlOutputBufferCreate(buffer)
+
+  local success = libxml2.xmlC14NExecute(self.document, cbwrapper, nil, mode,
+                                            prefixes, with_comments, output_buffer)
+
+  if success < 0 then
+    return nil, "failed to generate C14N string"
+  end
+  return libxml2.xmlBufferGetContent(buffer)
+end
 
 
 return C14n

c962b94cd016f3309ca2afe19665ef9268a1cb04
Thijs Schreijer <thijs@thijsschreijer.nl>
2024-11-25 14:02:34 +0100

    use definitions as-is in xlm-io.lua

--- xmlua/libxml2/xml-io.lua
+++ xmlua/libxml2/xml-io.lua
@@ -2,27 +2,49 @@ local ffi = require("ffi")
 
 ffi.cdef[[
 
-typedef int (*xmlOutputWriteCallback) (void * context, const char * buffer, int len);
+/**
+ * xmlOutputWriteCallback:
+ * @context:  an Output context
+ * @buffer:  the buffer of data to write
+ * @len:  the length of the buffer in bytes
+ *
+ * Callback used in the I/O Output API to write to the resource
+ *
+ * Returns the number of bytes written or -1 in case of error
+ */
+ typedef int (*xmlOutputWriteCallback) (void * context, const char * buffer,
+ int len);
+/**
+* xmlOutputCloseCallback:
+* @context:  an Output context
+*
+* Callback used in the I/O Output API to close the resource
+*
+* Returns 0 or -1 in case of error
+*/
 typedef int (*xmlOutputCloseCallback) (void * context);
 
 struct _xmlOutputBuffer {
-   void*                   context;
-   xmlOutputWriteCallback  writecallback;
-   xmlOutputCloseCallback  closecallback;
+  void*                   context;
+  xmlOutputWriteCallback  writecallback;
+  xmlOutputCloseCallback  closecallback;
 
-   xmlCharEncodingHandlerPtr encoder; /* I18N conversions to UTF-8 */
+  xmlCharEncodingHandlerPtr encoder; /* I18N conversions to UTF-8 */
 
-   xmlBufferPtr buffer;    /* Local buffer encoded in UTF-8 or ISOLatin */
-   xmlBufferPtr conv;      /* if encoder != NULL buffer for output */
-   int written;            /* total number of byte written */
-   int error;
+  xmlBufPtr buffer;    /* Local buffer encoded in UTF-8 or ISOLatin */
+  xmlBufPtr conv;      /* if encoder != NULL buffer for output */
+  int written;            /* total number of byte written */
+  int error;
 };
 
-typedef struct _xmlOutputBuffer xmlOutputBuffer;
-typedef xmlOutputBuffer *xmlOutputBufferPtr;
+xmlOutputBufferPtr
+	xmlAllocOutputBuffer		(xmlCharEncodingHandlerPtr encoder);
 
-xmlOutputBufferPtr xmlAllocOutputBuffer (xmlCharEncodingHandlerPtr encoder);
-xmlOutputBufferPtr xmlOutputBufferCreateBuffer	(xmlBufferPtr buffer, xmlCharEncodingHandlerPtr encoder);
-int xmlOutputBufferClose	(xmlOutputBufferPtr out);
+xmlOutputBufferPtr
+	xmlOutputBufferCreateBuffer	(xmlBufferPtr buffer,
+					 xmlCharEncodingHandlerPtr encoder);
+
+int
+	xmlOutputBufferClose		(xmlOutputBufferPtr out);
 
 ]]
